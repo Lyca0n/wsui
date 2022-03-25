6436c15b6d47fa7c26d3e6fbd1b6c4692456ae7e
Lyca0n <julio_cesar_villalva@hotmail.com>
2022-03-24 18:57:05 -0700

    remove unecessary opt attr

--- model/options.go
+++ model/options.go
@@ -4,12 +4,9 @@ type Options struct {
 	OriginHeader string `json:"Origin-Header"`
 	SendAs       string `json:"Send-Content-As"`
 	ConsumeAs    string `json:"Get-Content-As"`
-	Reconnect    bool   `json:"AutoReconnect"`
 }
 
 func DefaultOpts() *Options {
-	var ops = &Options{
-		Reconnect: true,
-	}
+	var ops = &Options{}
 	return ops
 }
